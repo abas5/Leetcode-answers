2f34402fdac916fdadee5e8d6117e935895a4438
abas3 <asheikh3@uncc.edu>
2021-11-29 15:54:46 -0500

    daily coding challenge review

---
+++ review_three_number_sum.py
@@ -0,0 +1,20 @@
+def threeNumberSum(array, targetSum):
+    # Write your code here.
+    array.sort()
+	triplets = []
+	for i in range(len(array) - 2):
+		left = i + 1
+		right = len(array) - 1
+		while left < right:
+			current_sum = array[i] + array[left] + array[right]
+			if current_sum == targetSum:
+				triplets.append([array[i], array[left], array[right]])
+				left += 1
+				right -= 1
+			elif current_sum < targetSum:
+				left += 1
+			elif current_sum > targetSum:
+				right -= 1
+	return triplets
+
+# O(n^2) time and O(n) Space
