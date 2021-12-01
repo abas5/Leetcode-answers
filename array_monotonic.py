d3304874f1a2097822b7bff2456f67d046030886
abas3 <asheikh3@uncc.edu>
2021-11-30 19:33:40 -0500

    solve array monotonic question

---
+++ array_monotonic.py
@@ -0,0 +1,13 @@
+# O(n) time and O(1) space
+def isMonotonic(array):
+    # Write your code here.
+    isNonDecreasing = True
+	isNonIncreasing = True
+	
+	for i in range(1, len(array)):
+		if array[i] < array[i - 1]:
+			isNonDecreasing = False
+		if array[i] > array[i - 1]:
+			isNonIncreasing = False
+	
+	return isNonDecreasing or isNonIncreasing
