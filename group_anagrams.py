8642275dd9b4e96828cac3ef12463364a1038d34
abas3 <asheikh3@uncc.edu>
2021-12-01 11:09:45 -0500

    completed the group anagrams challenge

---
+++ group_anagrams.py
@@ -0,0 +1,14 @@
+def groupAnagrams(words):
+    # Write your code here.
+    anagrams = {}
+	
+	for word in words:
+		sortedWord = "".join(sorted(word))
+		if sortedWord in anagrams:
+			anagrams[sortedWord].append(word)
+		else:
+			anagrams[sortedWord] = [word]
+		
+	return list(anagrams.values())
+
+# log(n) since sorting Time and O(wn) space
