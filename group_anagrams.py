5c6bc57b082a56a04d2025e5eea6b32bf8d98abd
abas3 <asheikh3@uncc.edu>
2021-12-01 11:13:55 -0500

    add sort/sorted info

--- group_anagrams.py
+++ group_anagrams.py
@@ -12,3 +12,7 @@ def groupAnagrams(words):
 	return list(anagrams.values())
 
 # log(n) since sorting Time and O(wn) space
+
+
+# in python sorted() returns a list of sorted chars, ints
+# while object.sort --> sorts it in place and returns nothing
