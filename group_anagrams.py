4f4164335048f9ff730e4f8437b196b6a59e8ee7
abas5 <abassheikh33@gmail.com>
2022-02-22 11:33:03 -0500

    review group anagrams

--- group_anagrams.py
+++ group_anagrams.py
@@ -16,3 +16,22 @@ def groupAnagrams(words):
 
 # in python sorted() returns a list of sorted chars, ints
 # while object.sort --> sorts it in place and returns nothing
+
+
+def groupAnagrams(words):
+    # Write your code here.
+    # aaccbb and ccbbaa
+	# aabbcc and aabbcc == we add both to the dict
+	# aabbcc is the key and the word is the value, where value can be a list
+	
+	anagrams = {}
+	
+	for word in words:
+		sorted_word = "".join(sorted(word))
+		
+		if sorted_word in anagrams:
+			anagrams[sorted_word].append(word)
+		else:
+			anagrams[sorted_word] = [word]
+	
+	return list(anagrams.values())
